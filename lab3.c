e5d5ab5c64893cacdd5600d0369f31eefc6f23e0
Amirreza Shirazi <ashir025@sledge.cs.ucr.edu>
2021-08-27 16:30:25 -0700

    final version

--- lab3.c
+++ lab3.c
@@ -1,31 +1,34 @@
 #include "types.h"
 #include "user.h"
 
-// Prevent this function from being optimized, which might give it closed form
 #pragma GCC push_options
-#pragma GCC optimize ("O0")
+#pragma GCC optimize("O0")
 static int
-recurse(int n)
+recursive(int n_start)
 {
-  if(n == 0)
+  if (n_start == 0)
     return 0;
-  return n + recurse(n - 1);
+  return n_start + recursive(n_start - 1);
 }
 #pragma GCC pop_options
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  int n, m;
-
-  if(argc != 2){
-    printf(1, "Usage: %s levels\n", argv[0]);
-    exit();
+  int m;
+  int n_start = 9;
+  int i = 0;
+  int new_n = 0;
+  int x = 1;
+  printf(1, "The program is starting at 9 levels and incrementing in multiplies of 9\n");
+  for (i = 0; i < 4; i++)
+  {
+    printf(1, "Iteration: # %d\n", x);
+    printf(1, "This Program is Recursing at %d levels\n", n_start);
+    m = recursive(n_start);
+    printf(1, "The Yielded value is %d\n", m);
+    new_n = n_start * 9;
+    n_start = new_n;
+    x++;
   }
-
-  n = atoi(argv[1]);
-  printf(1, "Lab 3: Recursing %d levels\n", n);
-  m = recurse(n);
-  printf(1, "Lab 3: Yielded a value of %d\n", m);
   exit();
 }
